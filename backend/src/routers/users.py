534738305bd20417ae9c2d043764b0a276327e11
王羿程 <ryan940609@gmail.com>
2024-11-06 22:20:35 +0800

    refactor: Move the API with the path /api/v1/users to src/routers/users.py.

---
+++ backend/src/routers/users.py
@@ -0,0 +1,46 @@
+# Python standard library
+from datetime import timedelta
+
+# Third-party imports
+from fastapi import APIRouter, Depends
+from fastapi.security import OAuth2PasswordRequestForm
+from sqlalchemy.orm import Session
+
+# Local application imports
+from src.auth.services import (
+    authenticate_user_token,
+    create_access_token,
+    check_user_password_is_correct,
+    pwd_context
+)
+from src.dependencies import session_opener
+from src.models import User
+from src.schemas import UserAuthSchema
+
+router = APIRouter()
+
+@router.post("/api/v1/users/login")
+async def login_for_access_token(
+        form_data: OAuth2PasswordRequestForm = Depends(), database: Session = Depends(session_opener)
+):
+    """login"""
+    user = check_user_password_is_correct(database, form_data.username, form_data.password)
+    TOKEN_EXPIRE_MINUTES = 30
+    access_token = create_access_token(
+        data={"sub": str(user.username)}, expires_delta=timedelta(minutes=TOKEN_EXPIRE_MINUTES)
+    )
+    return {"access_token": access_token, "token_type": "bearer"}
+
+@router.post("/api/v1/users/register")
+def create_user(user: UserAuthSchema, database: Session = Depends(session_opener)):
+    """create user"""
+    hashed_password = pwd_context.hash(user.password)
+    new_user = User(username=user.username, hashed_password=hashed_password)
+    database.add(new_user)
+    database.commit()
+    database.refresh(new_user)
+    return new_user
+
+@router.get("/api/v1/users/me")
+def read_users_me(user=Depends(authenticate_user_token)):
+    return {"username": user.username}
