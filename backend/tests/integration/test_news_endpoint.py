e0223004481d69da037da5af0ae44a8df7bdec36
王羿程 <ryan940609@gmail.com>
2024-11-14 01:24:42 +0800

    fix: Upadte pytest.

--- backend/tests/integration/test_news_endpoint.py
+++ backend/tests/integration/test_news_endpoint.py
@@ -4,10 +4,11 @@ from sqlalchemy import create_engine, StaticPool
 from sqlalchemy.orm import sessionmaker
 import json
 from jose import jwt
-from main import app
-from main import Base, NewsArticle, User, session_opener, user_news_association_table
-from main import NewsSumaryRequestSchema, PromptRequest
-from main import pwd_context
+from src.main import app
+from src.models import Base, NewsArticle, User, user_news_association_table
+from src.schemas import NewsSumaryRequestSchema, PromptRequest
+from src.dependencies import session_opener
+from src.auth.services import pwd_context
 from unittest.mock import Mock
 
 
@@ -109,7 +110,7 @@ def test_read_user_news(test_user, test_token, test_articles):
     assert json_response[1]["is_upvoted"] is False
 
 def mock_openai(mocker, return_content):
-    mock_openai_client = mocker.patch('main.OpenAI')
+    mock_openai_client = mocker.patch('src.routers.news.OpenAI')
 
     mock_message = Mock()
     mock_message.content = return_content
@@ -127,11 +128,11 @@ def mock_openai(mocker, return_content):
 def test_search_news(mocker):
     mock_openai(mocker, "keywords")
 
-    mock_get_new_info = mocker.patch("main.get_new_info", return_value=[
+    mock_get_new_info = mocker.patch("src.routers.news.get_new_info", return_value=[
         {"titleLink": "http://example.com/news1"}
     ])
 
-    mock_get = mocker.patch("main.requests.get", return_value=mocker.Mock(
+    mock_get = mocker.patch("src.routers.news.requests.get", return_value=mocker.Mock(
         text="""
         <html>
         <h1 class="article-content__title">Test Title</h1>
