c879861a89adf8f5052f2c860eb947a653778d67
王羿程 <ryan940609@gmail.com>
2024-11-21 20:13:24 +0800

    [第9組]Fix/integration test (#12)
    * fix: Upadte pytest.
    
    * fix: Upadte ci
    
    * fix: Upadte ci

--- backend/tests/integration/test_user_endpoint.py
+++ backend/tests/integration/test_user_endpoint.py
@@ -2,10 +2,12 @@ import pytest
 from fastapi.testclient import TestClient
 from sqlalchemy import create_engine, StaticPool
 from sqlalchemy.orm import sessionmaker
-from main import app
-from main import Base, User, session_opener
+from src.main import app
+from src.database import Base
+from src.dependencies import session_opener
+from src.models import User
+from src.auth.services import pwd_context
 from jose import jwt
-from main import pwd_context
 
 SECRET_KEY = "1892dhianiandowqd0n"
 ALGORITHM = "HS256"
