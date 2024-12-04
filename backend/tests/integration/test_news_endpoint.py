1049697654b640e832133a89d605c3ff8b6d5b72
王羿程 <ryan940609@gmail.com>
2024-12-05 05:35:41 +0800

    fix: update old test to handle llm_client

--- backend/tests/integration/test_news_endpoint.py
+++ backend/tests/integration/test_news_endpoint.py
@@ -110,20 +110,11 @@ def test_read_user_news(test_user, test_token, test_articles):
     assert json_response[1]["is_upvoted"] is False
 
 def mock_openai(mocker, return_content):
-    mock_openai_client = mocker.patch('src.routers.news.OpenAI')
-
-    mock_message = Mock()
-    mock_message.content = return_content
-
-    mock_choice = Mock()
-    mock_choice.message = mock_message
-
-    mock_completion = Mock()
-    mock_completion.choices = [mock_choice]
-
-    mock_openai_client.return_value.chat.completions.create.return_value = mock_completion
-
-    return mock_openai_client
+    mock_openai_client = mocker.patch('src.routers.news.LLMClient')
+    try:
+        mock_openai_client.return_value.generate_summary.return_value = json.loads(return_content)
+    except:
+        mock_openai_client.return_value.extract_search_keywords.return_value = return_content
 
 def test_search_news(mocker):
     mock_openai(mocker, "keywords")
